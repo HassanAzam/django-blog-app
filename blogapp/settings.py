2421095045481baa63dce1cdfde5cf2cd204c69b
HassanAzam <hassanazam55@gmail.com>
2017-11-10 04:04:26 +0500

    Added send email functionality | SharePostsByEmail feature completed

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#SMTP Configuration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'yourgmailaccount@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourgmailpassword'
+EMAIL_USE_TLS = True
